0982cce87a9c411640bcf76bec18d69e75a61d4d
shariyarbhuyan-code <shariyarbhuyan@gmail.com>
2026-06-30 11:35:19 +0800

    Refactor comments in Main.java for better clarity
    Updated comments for clarity and added numbering.

--- Main.java
+++ Main.java
@@ -2,21 +2,21 @@ public class Main {
 
     public static void main(String[] args) {
         
-        // Create an instance of the Student class
+        // 1. Create an instance of the Student class
         Student student = new Student();
 
-        // Display initial student variables
+        // 2. Display initial student variables
         System.out.println("Student Name is " + student.name);
         System.out.println("Student id is " + student.id);
         System.out.println("Student is in " + student.className);
         System.out.println("Student is in " + student.program);
         System.out.println("is the student running?: " + student.isRunning);
 
-        // Call the start method
+        // 3. Call the start method and print updated state
         student.start();
         System.out.println("is the student running?: " + student.isRunning);
 
-        // Call the stop method
+        // 4. Call the stop method
         student.stop();
     }
 }
